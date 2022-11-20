13c6e52cbac22043c680d127cccdbbeea4c87193
= <srudra754@gmail.com>
2022-11-20 13:33:38 +0530

    cards and prfanity check

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const authRoutes = require('./routes/authRoutes')
 const {checkAuth} = require('./middlewares/checkAuth')
 const postRoutes = require('./routes/postsRoutes')
 
-const DB = "mongodb://127.0.0.1:27017/smartChat"
-// const DB = 'mongodb+srv://srudra754:test123@cluster0.zey6y.mongodb.net/smartChat?retryWrites=true&w=majority'
+// const DB = "mongodb://127.0.0.1:27017/smartChat"
+const DB = 'mongodb+srv://srudra754:test123@cluster0.zey6y.mongodb.net/smartChat?retryWrites=true&w=majority'
 mongoose.connect(DB,{useNewUrlParser: true,useUnifiedTopology: true,}).then(()=>{console.log("connected to db")}).catch(()=>{"error connecting"})
    
 
